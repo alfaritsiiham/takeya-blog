5f8b205c74b734233e8703a07b5b3b975b01f42f
alfaritsiiham <alfaritsiiham@gmail.com>
2025-03-03 21:27:47 +0700

    add "no data" label

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,41 +22,45 @@
                     <div class="space-y-6 p-6">
                         <h2 class="text-lg font-semibold">Your Posts</h2>
 
-                        @foreach($posts as $val)
-                            <div class="rounded-md border p-5 shadow">
-                                <div class="flex items-center gap-2">
+                        @if(count($posts) == 0)
+                            <center>No Data...</center>
+                        @else
+                            @foreach($posts as $val)
+                                <div class="rounded-md border p-5 shadow">
+                                    <div class="flex items-center gap-2">
 
-                                    @if($val->status == App\Models\Post::STATUS_PUBLISHED)
-                                        <span class="flex-none rounded bg-green-100 px-2 py-1 text-green-800">{{ $val->status }}</span>
-                                    @elseif($val->status == App\Models\Post::STATUS_DRAFT)
-                                        <span class="flex-none rounded bg-gray-100 px-2 py-1 text-gray-800">{{ $val->status }}</span>
-                                    @elseif($val->status == App\Models\Post::STATUS_SCHEDULED)
-                                        <span class="flex-none rounded bg-yellow-100 px-2 py-1 text-yellow-800">{{ $val->status }}</span>
-                                    @endif
-
-                                    <h3><a href="{{ route('posts.show', $val->slug) }}" class="text-blue-500">{{ $val->title }}</a></h3>
-                                </div>
-                                <div class="mt-4 flex items-end justify-between">
-                                    <div>
-                                        @if($val->status == App\Models\Post::STATUS_SCHEDULED)
-                                            <div>Scheduled: {{ $val->publish_date }}</div>
-                                        @else
-                                            <div>Published: {{ $val->publish_date ?? '-' }}</div>
+                                        @if($val->status == App\Models\Post::STATUS_PUBLISHED)
+                                            <span class="flex-none rounded bg-green-100 px-2 py-1 text-green-800">{{ $val->status }}</span>
+                                        @elseif($val->status == App\Models\Post::STATUS_DRAFT)
+                                            <span class="flex-none rounded bg-gray-100 px-2 py-1 text-gray-800">{{ $val->status }}</span>
+                                        @elseif($val->status == App\Models\Post::STATUS_SCHEDULED)
+                                            <span class="flex-none rounded bg-yellow-100 px-2 py-1 text-yellow-800">{{ $val->status }}</span>
                                         @endif
-                                        <div>Updated: {{ $val->updated_at }}</div>
+
+                                        <h3><a href="{{ route('posts.show', $val->slug) }}" class="text-blue-500">{{ $val->title }}</a></h3>
                                     </div>
-                                    <div>
-                                        <a href="{{ route('posts.show', $val->slug) }}" class="text-blue-500">Detail</a> /
-                                        <a href="{{ route('posts.edit', $val->slug) }}" class="text-blue-500">Edit</a> /
-                                        <form action="{{ route('posts.delete', $val->slug) }}" method="POST" class="inline">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button class="text-red-500">Delete</button>
-                                        </form>
+                                    <div class="mt-4 flex items-end justify-between">
+                                        <div>
+                                            @if($val->status == App\Models\Post::STATUS_SCHEDULED)
+                                                <div>Scheduled: {{ $val->publish_date }}</div>
+                                            @else
+                                                <div>Published: {{ $val->publish_date ?? '-' }}</div>
+                                            @endif
+                                            <div>Updated: {{ $val->updated_at }}</div>
+                                        </div>
+                                        <div>
+                                            <a href="{{ route('posts.show', $val->slug) }}" class="text-blue-500">Detail</a> /
+                                            <a href="{{ route('posts.edit', $val->slug) }}" class="text-blue-500">Edit</a> /
+                                            <form action="{{ route('posts.delete', $val->slug) }}" method="POST" class="inline">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button class="text-red-500">Delete</button>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        @endforeach
+                            @endforeach
+                        @endif
 
                         <div>
                             {{ $posts->links() }}
